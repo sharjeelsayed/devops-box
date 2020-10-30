cf3f88bd5e13bb8e2082b64853ab08d25a8d0514
Sharjeel Sayed <sharjeelsayed@users.noreply.github.com>
2020-10-30 15:01:35 +0530

    Additonal VM Installation Commands

--- Vagrantfile
+++ Vagrantfile
@@ -1,10 +1,74 @@
-Vagrant.configure(2) do |config|
-	config.vm.define "devops-box" do |devbox|
-		devbox.vm.box = "bento/ubuntu-20.04"
-    		devbox.vm.provision "shell", path: "scripts/install.sh"
-    		devbox.vm.provider "virtualbox" do |v|
-    		  v.memory = 4096
-    		  v.cpus = 2
-    		end
+#Vagrant.configure(2) do |config|
+#	config.vm.define "devops-box" do |devbox|
+#		devbox.vm.box = "bento/ubuntu-20.04"
+#    		devbox.vm.provision "shell", path: "scripts/install.sh"
+#    		devbox.vm.provider "virtualbox" do |v|
+#    		  v.memory = 4096
+#    		  v.cpus = 2
+#    		end
+#	end
+#end
+
+##########
+BOX_IMAGE = "bento/ubuntu-20.04"
+MEMORY = 1024
+CPU = 1
+Vagrant.configure("2") do |config|
+	#Optionally add a bridge network called public_network on en0
+	#config.vm.network "public_network", bridge: "en0: Wi-Fi (Wireless)"
+	config.vm.define "devops-box" do |devops-box|
+		devops-box.vm.box = BOX_IMAGE
+		devops-box.vm.provider "virtualbox" do |v|
+			v.name = "devops-box"
+			v.memory = MEMORY
+    		v.cpus = CPU
+		end
+		devops-box.vm.provision "shell", path: "scripts/install.sh"
+		devops-box.vm.provision "shell", inline: <<-SHELL
+			hostnamectl set-hostname devops-box
+			apt -y update && apt -y upgrade
+		SHELL
 	end
+
+	# Uncomment these lines to add an additional VM
+	config.vm.define "devops-box-additional" do |devops-box|
+		devops-box-additional.vm.box = BOX_IMAGE
+		devops-box-additional.vm.provider "virtualbox" do |v|
+			v.name = "devops-box-additional"
+			v.memory = MEMORY
+    		v.cpus = CPU
+		end
+		devops-box-additional.vm.provision "shell", path: "scripts/install.sh"
+		devops-box-additional.vm.provision "shell", inline: <<-SHELL
+			hostnamectl set-hostname devops-box-additional
+			apt -y update && apt -y upgrade
+		SHELL
+	end
+
+	
 end
+
+##########
+#References:
+#https://manski.net/2016/09/vagrant-multi-machine-tutorial
+#https://www.vagrantup.com/docs/cli/box
+#https://www.vagrantup.com/docs/cli/reload
+#https://www.vagrantup.com/docs/multi-machine
+#https://www.vagrantup.com/docs/networking/public_network
+#https://www.vagrantup.com/docs/providers/virtualbox/configuration
+#https://youtu.be/vBreXjkizgo #Vagrant Crash Course by  Traversy Media
+# vagrant box update
+#vagrant reload --provision
+# config.vm.box_check_update = false
+# config.vm.network "forwarded_port", guest: 80, host: 8080
+# config.vm.network "forwarded_port", guest: 80, host: 8080, host_ip: "127.0.0.1"
+# Create a private network, which allows host-only access to the machine
+# using a specific IP.
+# config.vm.network "private_network", ip: "192.168.33.10"
+# config.vm.synced_folder "../data", "/vagrant_data"
+# Example for VirtualBox:
+#
+# config.vm.provider "virtualbox" do |vb|
+#   # Display the VirtualBox GUI when booting the machine
+#   vb.gui = true
+# end
--- scripts/install.sh
+++ scripts/install.sh
@@ -14,7 +14,7 @@ fi
 && ssh-keygen -f /home/ubuntu/.ssh/mykey -N '' \
 && chown -R ubuntu:ubuntu /home/ubuntu/.ssh
 '
-hostnamectl set-hostname devops-box
+#hostnamectl set-hostname devops-box
 
 # Install packages
 if [ ${REDHAT_BASED} ] ; then
@@ -35,22 +35,51 @@ timedatectl set-timezone Asia/Kolkata #Set Time Zone to IST
 usermod -G docker vagrant
 
 # Install awscli and ebcli
-pip3 install -U awscli
-pip3 install -U awsebcli
+#pip3 install -U awscli
+#pip3 install -U awsebcli
+
+aws_cli_install()
+{
+  # Install awscli and ebcli
+  pip3 install -U awscli
+  pip3 install -U awsebcli  
+}
+
+terraform_install()
+{
+  TERRAFORM_VERSION=$(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r -M '.current_version')
+wget -q https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
+  && unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
+  && rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+}
+
+packer_install()
+{
+  # Packer Installation
+  PACKER_VERSION=$(curl -s https://checkpoint-api.hashicorp.com/v1/check/packer | jq -r -M '.current_version')
+  wget -q https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip \
+  && unzip -o packer_${PACKER_VERSION}_linux_amd64.zip -d /usr/local/bin \
+  && rm packer_${PACKER_VERSION}_linux_amd64.zip
+}
+
+aws_cli_install
+terraform_install
+packer_install
 
 # Terraform Installation
-TERRAFORM_VERSION=$(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r -M '.current_version')
-wget -q https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
-&& unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
-&& rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+#TERRAFORM_VERSION=$(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r -M '.current_version')
+#wget -q https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
+#&& unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
+#&& rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 
 # Packer Installation
-PACKER_VERSION=$(curl -s https://checkpoint-api.hashicorp.com/v1/check/packer | jq -r -M '.current_version')
-wget -q https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip \
-&& unzip -o packer_${PACKER_VERSION}_linux_amd64.zip -d /usr/local/bin \
-&& rm packer_${PACKER_VERSION}_linux_amd64.zip
+#PACKER_VERSION=$(curl -s https://checkpoint-api.hashicorp.com/v1/check/packer | jq -r -M '.current_version')
+#wget -q https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip \
+#&& unzip -o packer_${PACKER_VERSION}_linux_amd64.zip -d /usr/local/bin \
+#&& rm packer_${PACKER_VERSION}_linux_amd64.zip
 
 # clean up
 if [ ! ${REDHAT_BASED} ] ; then
   apt clean
 fi
+
